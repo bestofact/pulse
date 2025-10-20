081fd668138dc838309cd27de3d16105143ce0d2
Anıl Can Turan <anilcantrn@gmail.com>
2025-10-21 01:58:38 +0300

    git ignore cache fixed

--- build/build-release/CMakeFiles/compile_commands_link.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/compile_commands_link"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/compile_commands_link.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
